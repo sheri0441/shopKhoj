a5a02e116df3ea6d39130e334ebb55c11dc5f301
Shehari931 <91692552+Shehari931@users.noreply.github.com>
2023-01-12 04:34:37 +0500

    adding first file

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// header bg changer 
+const header = document.getElementsByClassName("header-style");
+console.log(header[0]);
+
+const headerBg = () => {
+    if (
+        window.scrollY === 0
+        ) {
+            header[0].style.background = "transparent";
+            console.log('working');
+        } else {
+            header[0].style.background = "#000" ;
+            console.log('Notworking');
+        }
+
+}
+    
+
+window.addEventListener('scroll', headerBg)
+
+// slider functional code
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+let i;
+const x = document.getElementsByClassName("slide");
+const dots = document.getElementsByClassName("slidePoint");
+if (n > x.length) {slideIndex = 1}
+if (n < 1) {slideIndex = x.length}
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+}
+for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace("active", "");
+}
+x[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " active";
+}
