6c2c9d6c2919848b53afc32c28ef8e74f62ef14d
Shehari931 <91692552+Shehari931@users.noreply.github.com>
2023-01-20 03:57:29 +0500

    complete the get listed page and adding the city page adn make few changes in old files also

--- global.js
+++ global.js
@@ -15,47 +15,6 @@ const headerBg = () => {
 window.addEventListener('scroll', headerBg)
 
 
-// slider functional code
-
-let slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-let i;
-const x = document.getElementsByClassName("slide");
-const dots = document.getElementsByClassName("slidePoint");
-if (n > x.length) {slideIndex = 1}
-if (n < 1) {slideIndex = x.length}
-for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-}
-for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace("active", "");
-}
-x[slideIndex-1].style.display = "block";  
-dots[slideIndex-1].className += " active";
-}
-
-
-const sliderChanger = () =>{
-    setInterval(function () {
-        plusDivs(1);
-        sliderChanger;
-    }, 5000)
-
-} 
-
-
-window.addEventListener('load', sliderChanger)
-
 
 // Read more and Read less Button for not on the home page 
 
@@ -106,3 +65,5 @@ hamburger.addEventListener('click', check)
 // }
 
 // icon.addEventListener('click', openBar);
+
+
