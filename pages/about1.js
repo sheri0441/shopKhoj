9c8fbb8b9c48295e68b6185d8745ed0be327fe85
Shehari931 <91692552+Shehari931@users.noreply.github.com>
2023-02-07 01:27:08 +0500

    making new js file for about adding the second div to display on click

---
+++ pages/about1.js
@@ -0,0 +1,96 @@
+const header = document.getElementsByClassName("navBar");
+const header1 = document.getElementsByClassName("header-style");
+
+
+const headerBg = () => {
+    
+    if (
+        window.scrollY === 0
+        ) {
+            header[0].style.background = "transparent";
+            header1[0].style.marginTop = "0px";
+        } else {
+            header[0].style.background = "#9597A1" ;
+            header1[0].style.marginTop = "-30px" ;
+        }
+}
+
+window.addEventListener('scroll', headerBg);
+
+
+// navigation 
+const closeBtn = document.getElementById('menuClose');
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+const body = document.getElementsByTagName('body')[0]
+
+const hideMenu = () => {
+    menu.style.display= 'none'
+    body.style.overflow = 'auto'
+}
+
+const showMenu = () => {
+    menu.style.display= 'block'
+    body.style.overflow = 'hidden'
+}
+
+hamburger.addEventListener('click', showMenu)
+closeBtn.addEventListener('click', hideMenu)
+
+
+// city nav bar botton 
+const btn = document.getElementById('dropDown');
+const cityNav = document.getElementById('dropDown_city');
+
+
+const showNav =() => {
+    if(cityNav.style.display === "flex"){
+        cityNav.style.display = "none" 
+    } else {
+        cityNav.style.display = "flex"
+    }
+}
+
+
+
+btn.addEventListener('click', showNav);
+
+
+const left = document.getElementById('cardHorizontal_arrow-left');
+const right = document.getElementById('cardHorizontal_arrow-right');
+const horiContainer = document.getElementById('cardHorizontal_container')
+
+
+const rightMove = () => {
+    horiContainer.scrollLeft += 343;
+}
+const leftMove = () => {
+    horiContainer.scrollLeft -= 343;
+}
+
+
+
+left.addEventListener('click', leftMove)
+right.addEventListener('click', rightMove)
+
+
+
+const staffDetail = document.getElementsByClassName('staffDetail');
+const ceo = document.getElementById('ceo');
+const founder = document.getElementById('founder');
+const advisorContainer = document.getElementById('advisorContainer')
+
+
+const staffList = () => {
+    if(ceo.checked){
+        founder.style.display = 'block'
+        advisorContainer.style.display = 'none'
+    } else {
+        founder.style.display = 'none'
+        advisorContainer.style.display = 'block'
+    }
+}
+
+for (let i = 0; i < staffDetail.length; i++) {
+    staffDetail[i].addEventListener('change', staffList)
+};
